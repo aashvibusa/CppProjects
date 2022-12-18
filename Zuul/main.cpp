82850e6a5d2ae70e98e3ccf4ecae1fb52ab26d81
aashvibusa <hemal@DESKTOP-2P1BVJN.localdomain>
2022-12-17 19:18:51 -0800

    room functions and declarations in main

---
+++ Zuul/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "room.h"
+
+using namespace std;
+
+int main() {
+
+  room entrance_hall, oak_hall, parlour, east_hall, smoking_room, spare_room, kitchen, dining_room, pantry, cellar, atrium, storage, study, bedroom, west_hall, back_hall, guest_room, back_room;
+  vector<item*> inventory;
+
+  //entrance_hall = new room("in the entrance hall of Bilbo's house at Bag End");
+}
---
+++ Zuul/room.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <unordered_map>
+#include <iomanip>
+#include "room.h" 
+
+using namespace std;
+
+struct item;
+
+room::room() {
+  roomDescription = new char[100];
+  itemDescription = new char[100];
+  direction = new char[30];
+}
+
+room::room(char *d) {
+  roomDescription = new char[100];
+  strcpy(roomDescription, d);
+}
+
+char* room::getDescription() {
+  return roomDescription;
+}
+
+void room::setExit(char *dir, room *r) {
+  direction = new char[30];
+  strcpy(direction, dir);
+  exits[direction] = r;
+}
+
+room* room::getExit(char *dir) {
+  direction = new char[30];
+  strcpy(direction, dir);
+  return exits.at(direction);
+}
+
+void room::setItem(char *n) {
+  itemDescription = new char[100];
+  strcpy(itemDescription, n);
+  item* i = new item;
+  i->description = itemDescription;
+  itemsList.push_back(i);
+}
+
+void room::removeItem(char *n) {
+  if(itemsList.size() != 0) {
+    for(vector<item*>::iterator it = itemsList.begin() ; it != itemsList.end(); )
+      if(strcmp((*it)->description, n) == 0) {
+	itemsList.erase(it);
+      }
+  } else {
+    cout << "none" << endl;
+  }
+}
+
+void room::getRoomDetails() {
+  cout << "Exits: ";
+  for(auto i = exits.begin(); i != exits.end(); i++)
+    cout << i->first << ", ";
+}
