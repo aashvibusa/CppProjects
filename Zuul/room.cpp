a16f2214b1a5e7d24b1cef16840e95607a5285cc
aashvibusa <hemal@DESKTOP-2P1BVJN.localdomain>
2022-12-18 20:54:06 -0800

    initialized rooms, exits, and items

--- Zuul/room.cpp
+++ Zuul/room.cpp
@@ -15,7 +15,7 @@ room::room() {
   direction = new char[30];
 }
 
-room::room(char *d) {
+room::room(const char *d) {
   roomDescription = new char[100];
   strcpy(roomDescription, d);
 }
@@ -24,34 +24,34 @@ char* room::getDescription() {
   return roomDescription;
 }
 
-void room::setExit(char *dir, room *r) {
+void room::setExit(const char *dir, room *r) {
   direction = new char[30];
   strcpy(direction, dir);
   exits[direction] = r;
 }
 
-room* room::getExit(char *dir) {
+room* room::getExit(const char *dir) {
   direction = new char[30];
   strcpy(direction, dir);
   return exits.at(direction);
 }
 
-void room::setItem(char *n) {
+void room::setItem(const char *n) {
   itemDescription = new char[100];
   strcpy(itemDescription, n);
-  item* i = new item;
+  item *i = new item;
   i->description = itemDescription;
   itemsList.push_back(i);
 }
 
-void room::removeItem(char *n) {
+void room::removeItem(const char *n) {
   if(itemsList.size() != 0) {
     for(vector<item*>::iterator it = itemsList.begin() ; it != itemsList.end(); )
       if(strcmp((*it)->description, n) == 0) {
 	itemsList.erase(it);
       }
   } else {
-    cout << "none" << endl;
+    cout << "That item is not here!" << endl;
   }
 }
 
@@ -59,4 +59,11 @@ void room::getRoomDetails() {
   cout << "Exits: ";
   for(auto i = exits.begin(); i != exits.end(); i++)
     cout << i->first << ", ";
+
+  cout << "Items: ";
+  if(itemsList.size() != 0) {
+    for(int i = 0; i < itemsList.size(); i++) {
+      cout << itemsList.at(i)->description;
+    }
+  }
 }
